407fb3fecc4457b5d35e344c078a5902ce00b731
Rishikesan Kamaleswaran <rkamales@uthsc.edu>
2026-07-22 07:54:44 -0400

    Delete 99_create_sepsis3_outcomes_deprecated.sql

--- 99_create_sepsis3_outcomes_deprecated.sql
+++
@@ -1,4 +0,0 @@
--- Deprecated legacy Sepsis-3 outcomes script.
--- Use 42_create_sepsis3_outcomes_30d.sql after 41_create_sepsis3_collapsed_48h.sql.
-\echo '99_create_sepsis3_outcomes_deprecated.sql is deprecated. Use 42_create_sepsis3_outcomes_30d.sql.'
-\quit 3
